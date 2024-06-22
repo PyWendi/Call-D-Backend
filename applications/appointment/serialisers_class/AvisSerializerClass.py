c7166dff15679cbc4ca4470a48286580fc4547f5
PyWendi <rakotondranaivogilbert21@gmail.com>
2024-06-22 20:03:04 +0200

    Minor update to adjust api validation

--- applications/appointment/serialisers_class/AvisSerializerClass.py
+++ applications/appointment/serialisers_class/AvisSerializerClass.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from applications.authentication.serialisers_class.userSerializer import LawyerSerialiser
+from applications.authentication.serialisers_class.userSerializer import ShortLawyerSerializer
 from ..models import Avis
 from django.contrib.auth import get_user_model
 
 
 class AvisSerializer(serializers.ModelSerializer):
-    lawyer = LawyerSerialiser(many=False, read_only=True, required=False)
+    lawyer = ShortLawyerSerializer(many=False, read_only=True, required=False)
 
     class Meta:
         model = Avis
--- applications/authentication/serialisers_class/userSerializer.py
+++ applications/authentication/serialisers_class/userSerializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .domainSerializer import UserDomainSerializer
+from .regionSerializer import RegionSerializer
 from django.contrib.auth import get_user_model
 from ..models import Domain, CustomUser
 
@@ -12,17 +13,27 @@ class LawyerSerialiser(serializers.ModelSerializer):
     is_staff = serializers.BooleanField(write_only=True, required=False)
     is_superuser = serializers.BooleanField(write_only=True, required=False)
     date_joined = serializers.DateTimeField(read_only=True, required=False)
+    region = RegionSerializer(many=False)
 
     domains = UserDomainSerializer(many=True)
 
     class Meta:
         model = CustomUser()
-        fields = "__all__"
+        # fields = "__all__"
+        exclude =['groups', 'user_permissions']
 
     def create(self, validated_data):
         user = CustomUser().objects.create()
         user.save()
         return user
+
+
+class ShortLawyerSerializer(serializers.ModelSerializer):
+    profile_img = serializers.ImageField(read_only=True)
+
+    class Meta:
+        model = CustomUser()
+        fields = ['id', 'first_name', 'last_name', 'profile_img']
 
 
 class ClientSerialiser(serializers.ModelSerializer):
@@ -33,11 +44,12 @@ class ClientSerialiser(serializers.ModelSerializer):
     is_staff = serializers.BooleanField(write_only=True, required=False)
     is_superuser = serializers.BooleanField(write_only=True, required=False)
     date_joined = serializers.DateTimeField(read_only=True, required=False)
+    region = RegionSerializer(many=False)
 
     class Meta:
         model = CustomUser()
         # fields = "__all__"
-        exclude = ["domains", "availability"]
+        exclude = ["domains", "availability", 'groups', 'user_permissions']
 
     def create(self, validated_data):
         user = CustomUser
@@ -49,7 +61,7 @@ class ClientForNotificationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser()
-        fields = ["first_name", "last_name", 'profile_img']
+        fields = ["id", "first_name", "last_name", 'profile_img']
 
 
 class ProfileImageSerializer(serializers.ModelSerializer):
